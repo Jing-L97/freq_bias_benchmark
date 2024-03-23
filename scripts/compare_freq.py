e1db59fb874108533cedd628c7d008212b14a860
jliu <ecnucrystal@gmail.com>
2024-03-23 16:44:44 +0100

    compare freq

--- scripts/compare_freq.py
+++ scripts/compare_freq.py
@@ -1,85 +1,75 @@
 """
-compare the freq
+compare the freq between the generation and train set
 """
-
 import pandas as pd
 import os
 from tqdm import tqdm
 from lm_benchmark.compare_util import get_freq_table
 
+text_path = '/data/freq_bias_benchmark/data/generation/generated/merged/'
+out_path = '/data/freq_bias_benchmark/data/generation/gen_freq/'
+column_list = ['train','unprompted_0.3','unprompted_0.6','unprompted_1.0','unprompted_1.5']
 
-"""
-analyze the generated tokens
-"""
-new_path = '/data/freq_bias_benchmark/data/generation/generated/new/400h/02/unprompted/sample_random/'
-def merge_gen():
-    """merge old and new generations"""
-
-
-def get_freq():
+def get_freq(text_path:str, out_dir:str,column_list:list):
     """
-    get freq from the generated tokens
+    get freq from the train/generated tokens
+    input:
+        path: path to the text path
+        column_list: header list for the
     Returns
     -------
     freq table of the gen
     """
+    for file in tqdm(os.listdir(text_path)):
+        out_path = out_dir + file[:-4] + '/'
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
 
-    return
-def compare_freq(gen_path, train_path):
+        text = pd.read_csv(text_path + file)
+        # loop over different column header
+        for col in column_list:
+            freq = get_freq_table(text[col].tolist())
+            freq.to_csv(out_path + col + '_' + file)
+
+get_freq(text_path, out_path,column_list)
+
+
+
+def compare_freq(gen_path:str):
     '''
     map exp and filter freq: loop over the generated tokens
-    input: generation.csv; freq_table
-    return
-        - stat.csv for each model and temp
-        - freq stat for each model and temp
+    input: path to train adn gen freq
+    return updated gen csv
     '''
+    frame = pd.DataFrame()
+    train_freq = pd.read_csv(gen_path + 'train_' + gen_path.split('/')[-2] + '.csv')   # load train freq
+    # go over the train and gen freq df
+    for file in tqdm(os.listdir(gen_path)):
+        if not file.startswith('train'):
+            gen_freq = pd.read_csv(gen_path + file)
+            # match the freq info
+            n = 0
+            while n < gen_freq.shape[0]:
+                gen_row = gen_freq.iloc[[n]]
+                try:
+                    selected_row = train_freq[train_freq['Word'] == gen_row['Word'].tolist()[n]]
+                    for header in ['Freq', 'Norm_freq', 'Norm_freq_per_million', 'Log_freq', 'Log_norm_freq_per_million']:
+                        gen_row['train_' + header] = selected_row[header].item()
+                except:
+                    # fill zeros to the current dataframe; fill in zeros to the dataframe
+                    selected_row = pd.DataFrame([0, 0, 0, 0, 0, 0]).T
+                    for header in ['Freq', 'Norm_freq', 'Norm_freq_per_million', 'Log_freq', 'Log_norm_freq_per_million']:
+                        gen_row['train_' + header] = 0
+                # concatenate the rows
+                frame = pd.concat([frame, selected_row])
+                n += 1
+        # output the appended df
+        frame.to_csv(gen_path + file)
+    return frame
 
-    gen_frame = pd.read_csv(gen_path)
-    gen_frame['month'] = gen_frame['month'].astype(int)
-    # loop over the model list/month
-    frame_all = pd.DataFrame()
-    for model, age_range in tqdm(month_dict.items()):
 
-        # get gen freq
-        gen_frame_selected = gen_frame[(gen_frame['month'] >= age_range[0]) & (gen_frame['month'] <= age_range[1])]
-
-        if not os.path.exists(out_path + '/freq/' + model + '_' + temp + '.csv'):
-            gen_freq = get_freq_table(gen_frame_selected['unprompted_' + str(temp)].tolist())
-            gen_freq.to_csv(out_path + '/freq/' + model + '_' + temp + '.csv')
-        else:
-            print('There exists the file ' + model + '_' + temp)
-            gen_freq = pd.read_csv(out_path + '/freq/' + model + '_' + temp + '.csv')
-        # get train freq
-        train_freq = pd.read_csv(train_path + model + '.csv')
-        train_freq = train_freq.loc[:, 'Word':]
-        # map token freq
-        frame = pd.DataFrame()
-        # match the freq
-        n = 0
-        while n < gen_freq.shape[0]:
-            gen_row = gen_freq.iloc[[n]]
-            try:
-                selected_row = train_freq[train_freq['Word'] == gen_row['Word'].tolist()[n]]
-            except:
-                # fill zeros to the current dataframe; fill in zeros to the dataframe
-                selected_row = pd.DataFrame([0, 0, 0, 0, 0, 0]).T
-                selected_row.columns = ['Word', 'Freq', 'Norm_freq', 'Norm_freq_per_million', 'Log_freq',
-                                        'Log_norm_freq_per_million']
-
-            frame = pd.concat([frame, selected_row])
-
-            n += 1
-
-        frame.to_csv(out_path + model + '_' + temp + '.csv')
-        # append model info
-        frame['model'] = model
-        frame['temp'] = temp
-        frame_all = pd.concat([frame_all, frame])
-
-    # output the df
-    frame_all.to_csv(out_path + temp + '.csv')
-    return frame_all
-
+gen_path = '/data/freq_bias_benchmark/data/generation/gen_freq/400/'
+compare_freq(gen_path)
 
 """
 plot the results for the generatred tokens
