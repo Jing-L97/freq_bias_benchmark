927bf6ef33410a121c5f35bc652bdd15d64bdfa7
jliu <ecnucrystal@gmail.com>
2024-03-25 13:48:06 +0100

    constrcut oov sets

--- scripts/estimate_oov.py
+++ scripts/estimate_oov.py
@@ -11,39 +11,67 @@ filename_path = '/data/freq_bias_benchmark/data/train/filename/7100.csv'
 train_freq_dir = '/data/freq_bias_benchmark/data/train/train_freq/'
 text_dir = '/data/Machine_CDI/Lexical-benchmark_data/train_phoneme/dataset/'
 out_dir = '/data/freq_bias_benchmark/data/train/oov/train_utt/'
+
+
+def remove_file(large_list,sublist_to_remove):
+    # Find the index where the sublist to remove starts
+    start_index = large_list.index(sublist_to_remove[0])
+    # Remove the sublist from the large list
+    large_list = large_list[:start_index] + large_list[start_index + len(sublist_to_remove):]
+    return large_list
+
+
+def cut_df(df,target_cum_sum):
+    """cut df rows until it has reached the target value"""
+
+    # Calculate cumulative sum
+    cum_sum = df['num_tokens'].cumsum()
+    # Find the index where the cumulative sum exceeds or equals the target value
+    index_to_cut = cum_sum[cum_sum >= target_cum_sum].index.min()
+    # If no index found, keep all rows
+    if pd.isnull(index_to_cut):
+        index_to_cut = len(df)
+    # Remove rows after the index_to_cut
+    df = df.iloc[:index_to_cut]
+    return df
+
+
 def get_oov_mat(filename_path:str, train_freq_dir:str, text_dir:str,out_dir:str):
 
     """
-
+    constructhe target pseudo dataset to estimate oov token freq
     """
     def count_token(text):
         return len(text.split())
 
     # read train filename
     file_lst = pd.read_csv(filename_path,header=None)[0].tolist()
-
+    # get the list of the candidate txt file
+    all_file_lst = os.listdir(text_dir)
+    candi_lst = remove_file(all_file_lst,file_lst)
     # loop train_freq file
     for file in tqdm(os.listdir(train_freq_dir)):
         # count token numbers
         train_num = pd.read_csv(train_freq_dir + file)['Freq'].sum()
         oov_sum = 0
         train_frame = pd.DataFrame()
-        
+        # get constructed set
+        n = 0
         while oov_sum < train_num:
-            for txt in tqdm(os.listdir(text_dir)):
-                if txt not in file_lst:
-                    # get the token num
-                    with open(text_dir + txt, encoding="utf8") as f:
-                        lines = f.readlines()
-                        cleaned_lines = clean_text(lines)
-                        frame = pd.DataFrame(cleaned_lines)
-                        # assign column headers
-                        frame = frame.rename(columns={0: 'train'})
-                        frame['num_tokens'] = frame['train'].apply(count_token)
-                        frame.insert(loc=0, column='filename', value=file)
-                        oov_sum += frame['num_tokens'].sum()
-                        train_frame = pd.concat([train_frame,frame])
-
+            txt = candi_lst[n]
+            with open(text_dir + txt, encoding="utf8") as f:
+                lines = f.readlines()
+                cleaned_lines = clean_text(lines)
+                frame = pd.DataFrame(cleaned_lines)
+                # assign column headers
+                frame = frame.rename(columns={0: 'train'})
+                frame['num_tokens'] = frame['train'].apply(count_token)
+                frame.insert(loc=0, column='filename', value=txt)
+                oov_sum += frame['num_tokens'].sum()
+                train_frame = pd.concat([train_frame,frame])
+                n += 1
+        # cut additional line to align with the target train set
+        train_frame = cut_df(train_frame,train_num)
         # print out the utt
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
@@ -51,3 +79,6 @@ def get_oov_mat(filename_path:str, train_freq_dir:str, text_dir:str,out_dir:str)
 
 
 get_oov_mat(filename_path, train_freq_dir, text_dir,out_dir)
+
+
+
